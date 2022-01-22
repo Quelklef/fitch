77fc7479f4a4a88395c40816e9eff725725eddc8
Maynard <elimaynard923@gmail.com>
2022-01-21 23:46:28 -0600

    fix derivation

--- nix/nix.nix
+++ nix/nix.nix
@@ -20,28 +20,18 @@ nixed = purs-nix.purs
       ];
   };
 
-node_modules = npmlock2nix.node_modules { src = gitignoreSource ./..; };
-
 in {
 
   deriv = pkgs.stdenv.mkDerivation {
     name = "fitch";
-    dontUnpack = true;
+    src = ./../app;
 
     buildInputs = [ pkgs.nodejs ];
 
     installPhase = ''
       mkdir -p $out
-
       cp ${nixed.modules.Main.bundle {}} $out/index.js
-
-      if [ -d ${node_modules}/node_modules ]; then
-        mkdir -p $out/node_modules/
-        cp -r ${node_modules}/node_modules $out/
-      fi
-
-      echo "${pkgs.nodejs}/bin/node $out/index.js" > $out/run.sh
-      chmod +x $out/run.sh
+      cp $src/{index.html,css.css,favicon.ico} $out
     '';
   };
 
